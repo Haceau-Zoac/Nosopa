5d0cd8620f8a9604f029a6c068d91c43387a9eb9
Haceau-Zoac <3343471185@qq.com>
2021-01-25 12:27:46 +0800

    Update codes.

---
+++ Nosopa/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.Net.Http
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main args =
+    
+    use client = new HttpClient()
+    if args.Length < 1 then
+        printfn "Using: Nosopa <Name>"
+        1
+    else
+        $"https://www.qidian.com/search?kw={args.[0]}"
+        |> client.GetStringAsync
+        |> fun t -> t.Result
+        |> fun s -> Regex.Matches(s, "<h4><a href=\"//book.qidian.com/info/\\d+\" target=\"_blank\"([\\s\\S]*?)</h4>")
+        |> Seq.map (fun m -> m.Value)
+        |> Seq.map (fun s -> Regex.Replace(s, "<[\\s\\S]*?>|</[\\s\\S]*?>", ""))
+        |> Seq.iter Console.WriteLine
+        0
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Nosopa
-爬取小说网站~
+一个简易的爬取[起点中文网](https://www.qidian.com/)搜索结果的爬虫(
+使用 F# 编写！
+仅用于学习:P
+以 GPLv3 协议开源！
+## 使用
+Nosopa <Name>
